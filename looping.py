57949a5319b7b7e214d0694e518ca67fd7c4bcd4
SigmaQuan <quan.zhibin@gmail.com>
2016-05-17 11:18:46 +0800

    theano scan function.

--- looping.py
+++ looping.py
@@ -223,31 +223,228 @@
 # function.
 
 # Using shared variables - Gibbs sampling
+#
+# # Another useful feature of scan, is that it can handle shared variables.
+# # For example, if we want to implement a Gibbs chain of length 10 we would
+# # do the following:
+# import numpy as np
+# import theano
+# from theano import tensor as T
+# # We assume that "W_values" contains the initial values of you weight
+# # matrix.
+# W_values = np.matrix('1 2; 3 4')
+# print W_values
+# W = theano.shared(W_values)
+# print W
+#
+# bvis_values = 1
+# bhid_values = 1
+# bvis = theano.shared(bvis_values)
+# bhid = theano.shared(bhid_values)
+#
+# trng = T.shared_randomstreams.RandomStreams(1234)
+#
+#
+# def OneStep(vsample):
+#     hmean = T.nnet.sigmoid(theano.dot(vsample, W) + bhid)
+#     hsample = trng.binomial(size=hmean.shape, n=1, p=hmean)
+#     vmean = T.nnet.sigmoid(theano.dot(hsample, W.T) + bvis)
+#     return trng.binomial(size=vsample.shape, n=1, p=vmean,
+#                          dtype=theano.config.floatX)
+#
+# sample = theano.tensor.vector()
+#
+# values, updates = theano.scan(OneStep, outputs_info=sample, n_steps=10)
+#
+# gibbs10 = theano.function([sample], values[-1], updates=updates)
+# print gibbs10
+#
+# # The first, and probably most crucial observation is that the updates
+# # dictionary becomes important in this case. It links a shared varible with
+# # its updated value after k steps. In this case it tells how the random
+# # streams get updated after 10 iterations. If you do not pass this update
+# # dictionary to your function, you will always get the same 10 sets of
+# # random numbers. You can even use the updates dictionary afterwards. Look
+# # at this example:
+# a = theano.shared(1)
+# values, updates = theano.scan(lambda: {a: a+1}, n_steps=10)
+#
+# # In this case the lambda expression does not require and input parameters
+# # and returns an update dictionary with tells how a should be updated after
+# # each step of scan. If we write:
+# b = a + 1
+# c = updates[a] + 1
+# f = theano.function([], [b, c], updates=updates)
+#
+# print b
+# print c
+# print a.get_value()
+# # *****
+# # We will see that because b does not use the updated version of a, it will
+# # be 2, c will be 12, while a.value is 11. If we call the function again, b
+# # will become 12, c will be 22 and a.value 21. If we do not pass the
+# # updates dictionary to the function, then a.value will always be 2 and c
+# # will always be 12.
 
-# Another useful feature of scan, is that it can handle shared variables.
-# For example, if we want to implement a Gibbs chain of length 10 we would
-# do the following:
+# The second observation is that if we use shared variables (W, bvis, bhid)
+# but we do not iterate over them (i.e. scan does not really need to know
+# anything in particular about them, just that they are used inside the
+# function applied at each step) you do not need to pass them as arguments.
+# Scan Op calling any earlier (external) Op over and over. This results in
+# a simpler computational graph. However, passing them to the scan function
+# is a good practice, as it avoids Scan Op calling any earlier (external)
+# Op over and over. This results in a simpler computational graph, which
+# speeds up the optimization and the execution. *****To pass the shared
+# variables to Scan you need to put them in a list and give it to the
+# non_sequences argument. Here is the Gibbs sampling code updated:
+import numpy as np
 import theano
 from theano import tensor as T
-# We assume that "W_values" contains the initial values of you weight
-# matrix.
+W_values = np.matrix('1 2; 3 4')
+print W_values
 W = theano.shared(W_values)
+print W
 
+bvis_values = 1
+bhid_valuds = 1
 bvis = theano.shared(bvis_values)
-bhid = theano.shared(bhid_values)
+bhid = theano.shared(bhid_valuds)
 
 trng = T.shared_randomstreams.RandomStreams(1234)
 
 
-def OneStep(vsample):
+# OneStep, with explicit use fo the shared variables (W, bvis, bhid)
+def OneStep(vsample, W, bvis, bhid):
     hmean = T.nnet.sigmoid(theano.dot(vsample, W) + bhid)
-    hsample = trng.binomia(size=hmean.shape, n=1, p=hmean)
+    hsample = trng.binomial(size=hmean.shape, n=1, p=hmean)
     vmean = T.nnet.sigmoid(theano.dot(hsample, W.T) + bvis)
     return trng.binomial(size=vsample.shape, n=1, p=vmean,
                          dtype=theano.config.floatX)
 
 sample = theano.tensor.vector()
 
-values, updates = theano.scan(OneStep, outputs_info=sample, n_steps=10)
+# The new scan, with the shared variables passed as non_sequences
+values, updates = theano.scan(
+    fn=OneStep,
+    outputs_info=sample,
+    non_sequences=[W, bvis, bhid],
+    n_steps=10
+)
 
 gibbs10 = theano.function([sample], values[-1], updates=updates)
+# sample_value = theano.tensor.vector('1,2')
+# print(gibbs10(sample_value))
+
+# Using shared variables - the strict flag
+
+# As we just saw, passing the shared variables to scan many result in a
+# simpler computational graph, which speeds up the optimization and the
+# execution. A good way to remember to pass every shared variable used
+# during scan is to use the strict flag. When set to true, scan assumes
+# that all the necessary shared variables in fn are passed as a part of
+# non_sequences. This has to be ensured by the user. Otherwise, it will
+# result in an error.
+
+# Using the previous Gibbs sampling example:
+# The new scan, using strict=True
+values, updates =  theano.scan(
+    fn=OneStep,
+    outputs_info=sample,
+    non_sequences=[W, bvis, bhid],
+    strict=True
+)
+
+# If you omit to pass W, bvis or bhid, as a non_sequence, it will result in
+# an error.
+
+
+# Multiple outputs, several taps values - Recurrent Neural Network wth scan
+
+# The example above showed simple uses of scan. However, scan also supports
+# referring not only to the prior result and the current sequence value,
+# but also looking back more than one step.
+#
+# This is needed, for example, to implement a RNN using scan. Assume that
+# our RNN is defined as follows:
+# x_{n} = \tanh(
+#   Wx_{n-1}+
+#   W^{in}_{1}u_{n}+
+#   W^{in}_{2}u_{n-4}+
+#   W^{feedback}y_{n-1}
+# )
+# y_{n} = W^{out}x_{n-3}
+# Note that this network is far from a classical recurrent network and
+# might be useless. The reason  we defined as such is to better illustrate
+# the features of scan.
+#
+# In this case we have a sequence over which we need to iterate u, and two
+# outputs x and y. To implement this with scan we first construct a
+# function that computes one iteration step:
+def oneStep(u_tm4, u_t, x_tm3, x_tm1,
+            y_tm1, W, W_in_1, W_in_2, W_feedback, W_out):
+    x_t = T.tanh(
+        theano.dot(x_tm1, W) +
+        theano.dot(u_t, W_in_1) +
+        theano.dot(u_tm4, W_in_2) +
+        theano.dot(y_tm1, W_feedback)
+    )
+    y_t = theano.dot(x_tm3, W_out)
+    return [x_t, y_t]
+# As naming convention for the variables we used a _tmb to mean a at t-b
+# and a_tpb to be a at b+b. Note the order in which the parameter are
+# given, and in which the result is returned. Try to respect
+# chronological order among the taps (time slices of sequences or outputs)
+# used. For scan is crucial only for the variables representing the
+# different time taps to be in the same order as the one in which these
+# taps should respect an order, but also variables, since this is how scan
+# figures out what should be represented by what. Given that we have all
+# the Theano avriables needed we construct our RNN as follows:
+W = T.matrix()
+W_in_1 = T.matrix()
+W_in_2 = T.matrix()
+W_feedback = T.matrix()
+W_out = T.matrix()
+
+u = T.matrix()  # it is a sequence of vectors
+# initial state of x has to be a matrix, since it has to cover x[-3]
+x0 = T.matrix()
+# y0 is just a vector since scan has only to provide y[-1]
+y0 = T.vector()
+
+([x_vals, y_vals], updates) = theano.scan(
+    fn=oneStep,
+    sequences=dict(input=u, taps=[-4, -0]),
+    outputs_info=[dict(initial=x0, taps=[-3,-1]), y0],
+    non_sequences=[W, W_in_1, W_in_2, W_feedback, W_out],
+    strict=True
+)  # for second input y, scan adds -1 in output_taps by default
+
+# Now x_vals and y_vals are symbolic variables pointing to the sequence of
+# x and y values generated by iterating over u. The sequence_taps,
+# output_taps give to scan information about what slices are exactly
+# needed. Note that if we want to use x[t-k] we do not need to also have
+# x[t-(k-1)] x[t-(k-2)], ..., but when applying the compiled function, the
+# numpy array given to represent this sequence should be large enough to
+# cover this values. Assume that we compile the above function, and we give
+# as u the array uvals = [0,,1,2,3,4,5,6,7,8]. By abusing notations, scan
+# will consider uval[0] as u[-4], and will start scaning from uvals[4]
+# towards the end.
+
+
+# Conditional ending of scan
+
+# Scan can also be used as a repeat-until block. In such a case scan will
+# stop when either the maximal number of iteration is reached, or the
+# provided condition evaluates to True.
+#
+# For an example, we will compute all powers of two smaller then some
+# provided value max_value.
+
+
+# As a rule, scan always, expects the condition to be the last thing
+# returned by the inner function, otherwise an error will be raised..
+
+
+
+
+
