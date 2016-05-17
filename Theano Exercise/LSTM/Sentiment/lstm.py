e0cf81b3148123f55b2b436d1ce0f9684192fc04
quanzhbin <quanzhibin@gmail.com>
2016-05-17 16:08:42 +0800

    Add RNN

--- Theano Exercise/LSTM/Sentiment/lstm.py
+++ Theano Exercise/LSTM/Sentiment/lstm.py
@@ -62,11 +62,11 @@ def unzip(zipped):
 def dropout_layer(state_before, use_noise, trng):
     proj = tensor.switch(
         use_noise,
-            (state_before *
-             trng.binomial(state_before.shape,
-                           p=0.5,
-                           n=1,
-                           dtype=state_before.dtype)),
+        (state_before *
+         trng.binomial(state_before.shape,
+                       p=0.5,
+                       n=1,
+                       dtype=state_before.dtype)),
         state_before*0.5
     )
     return proj
