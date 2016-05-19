fbdbc06654901282abb819f381c44398b9b185a4
quanzhbin <quanzhibin@gmail.com>
2016-05-19 21:32:22 +0800

    Theano exercises.

---
+++ Theano-Exercise/Part-03-Machine-Learning/06_Auto-Encoder/_19_autoencoder.py
@@ -0,0 +1,11 @@
+# Figure out why this script doesn't work. Run an experiment to verify
+# that your explanation is correct. You probably want to import a function
+# that you wrote as part of one of the earlier exercise.
+
+import numpy as np
+
+from theano import function
+from theano.printing import Print
+import theano.tensor as tensor
+
+
