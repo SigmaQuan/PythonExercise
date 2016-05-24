2af014ee2d1ef410065ee0247d82a4b371f2f56d
SigmaQuan <quan.zhibin@gmail.com>
2016-05-24 12:40:00 +0800

    Add two ways for initialize the weight matrices in DAM: uniform distribution and Gaussian distribution.

--- Theano-Exercise/Part-01-Basics/01_Building_Expressions/_02_vector_mat.py
+++ Theano-Exercise/Part-01-Basics/01_Building_Expressions/_02_vector_mat.py
@@ -23,8 +23,43 @@ def make_matrix():
 
 def elemwise_mul(a, b):
     """
-    Returns the elementwise product of a and b.
+    Returns the element-wise product of a and b.
     :param a: a theano matrix
     :param b: a theano matrix
-    :return: the elementwise product of a and b.
+    :return: the element-wise product of a and b.
     """
+    c = a*b
+    return c
+
+
+def matrix_vector_mul(a, b):
+    """
+    Return the matrix-vector product of a and b.
+    :param a: a theano matrix.
+    :param b: a theano vector.
+    :return: the matrix-vector product of a and b.
+    """
+    return a*b
+
+
+if __name__ == "__main__":
+    a = make_vector()
+    b = make_vector()
+    c = elemwise_mul(a, b)
+    d = make_matrix()
+    e = matrix_vector_mul(d, c)
+
+    f = function([a, b, d], e)
+
+    rng = np.random.RandomState([1, 2, 3])
+    a_value = rng.randn(5).astype(a.dtype)
+    b_value = rng.randn(5).astype(b.dtype)
+    c_value = a_value * b_value
+    d_value = rng.randn(5, 5).astype(d.dtype)
+    expected = np.dot(d_value, c_value)
+
+    actual = f([a_value, b_value, d_value], expected)
+
+    assert np.allclose(actual, expected)
+    print("SUCCESS!")
+
