2db7e378aac49a125fe9145be8c5e8ddd9b92e0f
quan <quan.zhibin@gmail.com>
2016-05-24 23:22:44 +0800

    Theano exercise codes are come from public websites.

--- Theano-Exercise/Part-02-Advanced/04_Symbolic/_11_grad.py
+++ Theano-Exercise/Part-02-Advanced/04_Symbolic/_11_grad.py
@@ -11,3 +11,13 @@ def grad_sum(x, y, z):
     :param z: a theano expression involving x and y.
     :return: dz/dx + dz/dy.
     """
+    return sum(tensor.grad(z, [x, y]))
+
+
+if __name__ == "__main__":
+    x = tensor.scalar()
+    y = tensor.scalar()
+    z = x + y
+    s = grad_sum(x, y, z)
+    assert s.eval({x:0, y:0}) == 2
+    print("SUCCESS!")
