e399306a14576da9d00e7dd33c689daf13bfb7d5
quan <quan.zhibin@gmail.com>
2016-05-25 00:13:33 +0800

    Theano exercise codes are come from public websites.

--- Theano-Exercise/Part-02-Advanced/04_Symbolic/_12_traverse.py
+++ Theano-Exercise/Part-02-Advanced/04_Symbolic/_12_traverse.py
@@ -5,4 +5,18 @@ from theano import tensor
 
 
 def arg_to_softmax(prob):
+    """
+    Oh no! Some has passed you probability output, "prob", of a softmax
+    function, and you want the unnormalized log probabilty -- the argument
+    to the softmax.
+    Verify that prob really is the output of a softmax. Raise a TypeError
+    if it is not.
+    If it is, return the argument to the softmax.
+    :param prob:
+    :return:
+    """
+
+
+if __name__ == "__main__":
+    x = np.ones((5, 4))
     
