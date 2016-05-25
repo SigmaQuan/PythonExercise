c68e5a1b400e8893a42b7d65fc1a139748c71ed4
quan <quan.zhibin@gmail.com>
2016-05-25 18:01:50 +0800

    Merge branch 'master' of /home/quan/Documents/Code/PythonExercise with conflicts.

--- Theano-Exercise/Part-02-Advanced/04_Symbolic/_12_traverse.py
+++ Theano-Exercise/Part-02-Advanced/04_Symbolic/_12_traverse.py
@@ -2,7 +2,6 @@
 # works!
 import numpy as np
 from theano import tensor
-from theano.gof import Variable
 
 
 def arg_to_softmax(prob):
@@ -16,48 +15,8 @@ def arg_to_softmax(prob):
     :param prob:
     :return:
     """
-    #***** NOT UNDERSTAND
-    if not isinstance(prob, Variable):
-        raise TypeError()
-
-    if prob.owner is None:
-        raise  TypeError()
-
-    owner = prob.owner
-
-    if not isinstance(owner.op, tensor.nnet.Softmax):
-        raise TypeError()
-
-    rval, = owner.inputs
-    #***** NOT UNDERSTAND
-
-    return rval
 
 
 if __name__ == "__main__":
     x = np.ones((5, 4))
-    try:
-        arg_to_softmax(x)
-        raise Exception("You should have raise an error.")
-    except TypeError:
-        pass
-
-    x = tensor.matrix()
-    try:
-        arg_to_softmax(x)
-        raise Exception("You should have raise an error.")
-    except TypeError:
-        pass
-
-    y = tensor.nnet.sigmoid(x)
-    try:
-        arg_to_softmax(y)
-        raise Exception("You should have raise an error.")
-    except TypeError:
-        pass
-
-    y = tensor.nnet.softmax(x)
-    rval = arg_to_softmax(y)
-    assert rval is x
-
-    print("SUCCESS!")
+    
